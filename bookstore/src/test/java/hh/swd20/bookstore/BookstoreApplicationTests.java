40fc5ca7e65a04963aa35d5f7a401997f0281a2c
Pauli Hemmi <lolled.mr@gmail.com>
2022-10-25 19:50:52 +0300

    fixes

--- bookstore/src/test/java/hh/swd20/bookstore/BookstoreApplicationTests.java
+++ bookstore/src/test/java/hh/swd20/bookstore/BookstoreApplicationTests.java
@@ -1,26 +1,13 @@
 package hh.swd20.bookstore;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import hh.swd20.bookstore.web.BookController;
-
-//@RunWith(SpringRunner.class) // JUnit4
-@ExtendWith(SpringExtension.class)   // JUnit5 eli Jupiter
 @SpringBootTest
-public class BookstoreApplicationTests {
+class BookstoreApplicationTests {
 
-	@Autowired
-	private BookController bookController;
-	
 	@Test
-	public void contextLoads() {
-		assertThat(bookController).isNotNull();
+	void contextLoads() {
 	}
 
 }
